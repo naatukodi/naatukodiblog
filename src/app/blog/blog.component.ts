bace45b2c9b29fb5e725daead3556ba8f2fc5f52
Tirumala Rao  S <tiru@live.in>
2025-01-24 18:50:18 +0530

    modified css

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {ActivatedRoute, Router, ROUTES} from '@angular/router';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
 
 declare var ng: any;
 
@@ -12,8 +13,15 @@ declare var ng: any;
 
 })
 export class BlogComponent implements OnInit {
-  ngOnInit() {}
+  blogPost: any;
+  currentUrl: string = window.location.href;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private scully: ScullyRoutesService) {}
+
+  ngOnInit(): void {
+    this.scully.available$.subscribe(routes => {
+      const slug = this.route.snapshot.paramMap.get('slug');
+      this.blogPost = routes.find(route => route.route === `/blog/${slug}`);
+    });
   }
 }
